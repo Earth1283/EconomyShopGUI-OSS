b83a97d6a26eabc31aa15f1e38169fbc96bf49f0
Earth1283 <142399154+Earth1283@users.noreply.github.com>
2026-03-26 19:28:51 +0800

    initial commit

---
+++ src/main/kotlin/io/github/Earth1283/economyShopGUIOSS/EconomyShopGUIOSS.kt
@@ -0,0 +1,14 @@
+package io.github.Earth1283.economyShopGUIOSS
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class EconomyShopGUIOSS : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
